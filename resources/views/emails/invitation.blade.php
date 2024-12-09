2ae79ea030c18266b48474fa78bbc548710ca65a
systembrooklyn <system.bbs.2024@gmail.com>
2024-12-09 15:26:32 +0200

    updating url

--- resources/views/emails/invitation.blade.php
+++ resources/views/emails/invitation.blade.php
@@ -6,7 +6,6 @@
     <h1>You have been invited to join the platform!</h1>
     <p>Company: {{ $companyName }}</p>
     <p>Click the link below to complete your registration:</p>
-    <a href="{{ url('/register?token=' . $invitation->token) }}">Complete Registration</a>
-
+    <a href="{{ url('http://192.168.1.29:8080/user-information?token=' . $invitation->token) }}">Complete Registration</a>
 </body>
 </html>
